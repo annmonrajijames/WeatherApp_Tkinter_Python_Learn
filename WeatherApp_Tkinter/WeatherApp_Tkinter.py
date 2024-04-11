29c10c0f3b3550f070df8591fa3c15987f6e594f
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-04-11 14:20:24 +0530

    Learn how to access elements in dictionery

--- WeatherApp_Tkinter/WeatherApp_Tkinter.py
+++ WeatherApp_Tkinter/WeatherApp_Tkinter.py
@@ -5,15 +5,17 @@ import json
 # https://docs.airnowapi.org/
 # Below is the generated URL
 # https://www.airnowapi.org/aq/observation/zipCode/historical/?format=application/json&zipCode=89129&date=2024-04-10T00-0000&distance=5&API_KEY=80599F15-A413-4492-9620-E4ED538110B3
-root = Tk()
-root.title('WeatherApp_Tkinter')
-root.geometry("400x400")
+# root = Tk()
+# root.title('WeatherApp_Tkinter')
+# root.geometry("400x400")
 
 try:
     api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/historical/?format=application/json&zipCode=89129&date=2024-04-10T00-0000&distance=5&API_KEY=80599F15-A413-4492-9620-E4ED538110B3")
     api = json.loads(api_request.content)
+    print("from api=",api)
+    print("from api=",api[0]['Category'])
 except Exception as e:
     api = "Error..."
 
-myLabel = Label(root, text=api).pack()
-root.mainloop()
+#myLabel = Label(root, text=api[0]['Category']).pack()
+#root.mainloop()
