5215b664b4712dccccff505a1465beb5ff87f2a8
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-04-11 15:09:56 +0530

    Printed Air quality of a place

--- WeatherApp_Tkinter/WeatherApp_Tkinter.py
+++ WeatherApp_Tkinter/WeatherApp_Tkinter.py
@@ -5,16 +5,18 @@ import json
 # https://docs.airnowapi.org/
 # Below is the generated URL
 # https://www.airnowapi.org/aq/observation/zipCode/historical/?format=application/json&zipCode=89129&date=2024-04-10T00-0000&distance=5&API_KEY=80599F15-A413-4492-9620-E4ED538110B3
-# root = Tk()
-# root.title('WeatherApp_Tkinter')
-# root.geometry("400x400")
+root = Tk()
+root.title('WeatherApp_Tkinter')
+root.geometry("400x400")
 
 try:
     api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/historical/?format=application/json&zipCode=89129&date=2024-04-10T00-0000&distance=5&API_KEY=80599F15-A413-4492-9620-E4ED538110B3")
     api = json.loads(api_request.content)
-    print("from api=",api)
+    city=api[0]['ReportingArea']
+    quality=api[0]['AQI']
+    category=api[0]['Category']['Name']
 except Exception as e:
     api = "Error..."
 
-#myLabel = Label(root, text=api[0]['Category']).pack()
-#root.mainloop()
+myLabel = Label(root, text=city + " Air quality "+str(quality)+" "+ category).pack()
+root.mainloop()
