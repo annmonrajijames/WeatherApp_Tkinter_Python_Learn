6258c286d085fd02ccf1004cd8c4456ec39bc0e0
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-04-11 15:17:34 +0530

    Other design works like color and size

--- WeatherApp_Tkinter/WeatherApp_Tkinter.py
+++ WeatherApp_Tkinter/WeatherApp_Tkinter.py
@@ -7,8 +7,8 @@ import json
 # https://www.airnowapi.org/aq/observation/zipCode/historical/?format=application/json&zipCode=89129&date=2024-04-10T00-0000&distance=5&API_KEY=80599F15-A413-4492-9620-E4ED538110B3
 root = Tk()
 root.title('WeatherApp_Tkinter')
-root.geometry("200x200")
-
+root.geometry("400x40")
+root.configure(background='green')
 try:
     api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/historical/?format=application/json&zipCode=89129&date=2024-04-10T00-0000&distance=5&API_KEY=80599F15-A413-4492-9620-E4ED538110B3")
     api = json.loads(api_request.content)
@@ -18,5 +18,5 @@ try:
 except Exception as e:
     api = "Error..."
 
-myLabel = Label(root, text=city + " Air quality "+str(quality)+" "+ category, font=("Helvetica")).pack()
+myLabel = Label(root, text=city + " Air quality "+str(quality)+" "+ category, font=("Helvetica, 20"), background="green").pack()
 root.mainloop()
