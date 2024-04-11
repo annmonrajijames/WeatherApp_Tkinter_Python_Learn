d1d59497ed01f7a4ebfafc04134b9ca87f6c6b54
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-04-11 16:01:38 +0530

    Set colors-weather color for category

--- WeatherApp_Tkinter/WeatherApp_Tkinter.py
+++ WeatherApp_Tkinter/WeatherApp_Tkinter.py
@@ -4,19 +4,32 @@ import json
 # I used this below website to get API service
 # https://docs.airnowapi.org/
 # Below is the generated URL
-# https://www.airnowapi.org/aq/observation/zipCode/historical/?format=application/json&zipCode=89129&date=2024-04-10T00-0000&distance=5&API_KEY=80599F15-A413-4492-9620-E4ED538110B3
+# https://www.airnowapi.org/aq/observation/zipCode/historical/?format=application/json&zipCode=83814&date=2024-04-10T00-0000&distance=5&API_KEY=80599F15-A413-4492-9620-E4ED538110B3
 root = Tk()
 root.title('WeatherApp_Tkinter')
 root.geometry("400x40")
-root.configure(background='green')
 try:
     api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/historical/?format=application/json&zipCode=89129&date=2024-04-10T00-0000&distance=5&API_KEY=80599F15-A413-4492-9620-E4ED538110B3")
     api = json.loads(api_request.content)
     city=api[0]['ReportingArea']
     quality=api[0]['AQI']
     category=api[0]['Category']['Name']
+    if category == "Good":
+        weather_color="green"
+    elif category == "Moderate":
+        weather_color="#FFFF00"
+    elif category == "Unhealthy for Sensitive Groups":
+        weather_color="ff9900"
+    elif category == "Unhealthy":
+        weather_color="FF0000"
+    elif category == "Very Healthy":
+        weather_color="#990066"
+    elif category == "Hazardous":
+        weather_color="#660000"
+    
+    root.configure(background='green')
+    myLabel = Label(root, text=city + " Air quality "+str(quality)+" "+ category, font=("Helvetica, 20"), background=weather_color).pack()
 except Exception as e:
     api = "Error..."
 
-myLabel = Label(root, text=city + " Air quality "+str(quality)+" "+ category, font=("Helvetica, 20"), background="green").pack()
 root.mainloop()
