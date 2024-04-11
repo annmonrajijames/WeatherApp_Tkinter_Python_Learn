0233085196e5a603549f41c52990509aa411613f
Annmon Raji James <annmonjameschiramel@gmail.com>
2024-04-11 12:40:28 +0530

    API service website, just to remember

--- WeatherApp_Tkinter/WeatherApp_Tkinter.py
+++ WeatherApp_Tkinter/WeatherApp_Tkinter.py
@@ -1,9 +1,11 @@
 from tkinter import *
 import requests
 import json
-
+# I used this below website to get API service
+# https://docs.airnowapi.org/
 root = Tk()
 root.title('WeatherApp_Tkinter')
 root.geometry("400x400")
+
 
 root.mainloop()
